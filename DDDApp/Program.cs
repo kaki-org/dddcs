43c14866ae7e76c4ad0ff1ac0ede1d54350dcd79
kakikubo <kakikubo@gmail.com>
2025-09-24 10:19:28 +0900

    middleNameを追加して比較処理自体も改修

--- DDDApp/Program.cs
+++ DDDApp/Program.cs
@@ -1,9 +1,9 @@
 ﻿// FullNameクラスのLastNameプロパティを利用する
-var fullName = new FullName("teruo", "kakikubo");
+var fullName = new FullName("teruo", "kakikubo", "");
 Console.WriteLine(fullName.LastName); // kakikuboが表示される
 
 // 確実に姓を表示できる
-fullName = new FullName("john", "smith");
+fullName = new FullName("john", "smith", "thomas");
 Console.WriteLine(fullName.LastName); // smithが表示される
 
 // 数字の変更
@@ -18,7 +18,7 @@ Console.WriteLine(greet); // こんにちは　が表示される
 greet = "hello";
 Console.WriteLine(greet); // hello が表示される
 // 値オブジェクトの変更
-var fullName2 = new FullName("teruo", "kakikubo");
+var fullName2 = new FullName("teruo", "kakikubo", "");
 Console.WriteLine(fullName2); // kakiが表示される
 
 // 同じ種類の値同士の比較
@@ -29,8 +29,8 @@ Console.WriteLine('a' == 'b'); // False
 Console.WriteLine("hello" == "hello"); // True
 Console.WriteLine("hello" == "こんにちは"); // False
 // 値オブジェクト同士の比較
-var nameA = new FullName("John", "Smith");
-var nameB = new FullName("John", "Smith");
+var nameA = new FullName("John", "Smith", "Thomas");
+var nameB = new FullName("John", "Smith", "Thomas");
 // 別個のインスタンス同士の比較
 Console.WriteLine("nameAとnameBのEqualsの比較" + nameA.Equals(nameB)); // Trueになる
 // 演算子のオーバーライド機能を活用することも選択肢に入る
@@ -39,22 +39,25 @@ Console.WriteLine(compareResult2);
 
 
 // 氏名を表現するFullNameクラス
-class FullName
+class FullName : IEquatable<FullName>
 {
-    public FullName(string firstName, string lastName)
+    public FullName(string firstName, string lastName, string middleName)
     {
         FirstName = firstName;
-        LastName =  lastName;
+        LastName = lastName;
+        MiddleName = middleName;
     }
-    
+
     public string FirstName { get; }
     public string LastName { get; }
+    public string MiddleName { get; }
 
     public bool Equals(FullName other)
     {
         if (ReferenceEquals(null, other)) return false;
         if (ReferenceEquals(this, other)) return true;
-        return string.Equals(FirstName, other.FirstName) && string.Equals(LastName, other.LastName);
+        return string.Equals(FirstName, other.FirstName) && string.Equals(LastName, other.LastName) &&
+               string.Equals(MiddleName, other.MiddleName);
     }
 
     public override bool Equals(object obj)
@@ -64,12 +67,14 @@ class FullName
         if (obj.GetType() != this.GetType()) return false;
         return Equals((FullName)obj);
     }
+
     // C#ではEqualsをoverrideする際にGetHashCodeをoverrideするルールがある
     public override int GetHashCode()
     {
         unchecked
         {
-            return ((FirstName != null ? FirstName.GetHashCode() : 0) * 397) ^ (LastName != null ? LastName.GetHashCode() : 0);
+            return ((FirstName != null ? FirstName.GetHashCode() : 0) * 397) ^
+                   (LastName != null ? LastName.GetHashCode() : 0);
         }
     }
 }
