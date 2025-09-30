4ada5a89292250403f93c3d22e4f806ae9717997
kakikubo <kakikubo@gmail.com>
2025-09-30 18:52:44 +0900

    ユーザ名を設定してインスタンスを生成させる

--- DDDApp/User.cs
+++ DDDApp/User.cs
@@ -3,6 +3,13 @@ public class User
     public UserId Id { get; set; }
     public UserName Name { get; set; }
 
+    public User(string name)
+    {
+        if (name == null) throw new ArgumentNullException(nameof(name));
+        if (name.Length < 3) throw new ArgumentException("ユーザ名は3文字以上です",  nameof(name));
+        
+        this.Name = new UserName(name);
+    }
     public override string ToString()
     {
         return "Id: " +  Id.ToString() + ", Name: " + Name.ToString();
