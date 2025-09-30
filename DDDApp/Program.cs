2e6ad13dd0097402878825572ca06e38bacca843
kakikubo <kakikubo@gmail.com>
2025-10-01 07:30:07 +0900

    idとnameを渡すようなクラスに改修したので呼び出し方も変える

--- DDDApp/Program.cs
+++ DDDApp/Program.cs
@@ -60,12 +60,12 @@ Console.WriteLine(validUserName);
 
 var user = CreateUser(validUserName);
 Console.WriteLine(user);
-user.ChangeName("kay");
+user.ChangeUserName(new UserId("1"),"kay");
 Console.WriteLine(user);
 
 User CreateUser(UserName name)
 {
-    var user = new User("kakikubo");
+    var user = new User(new UserId("1"), "kakikubo");
     // user.Id = name; // 代入でミスっていても事前にエラーとして気付ける(ソースの型 'UserName' をターゲットの型 'UserId' に変換できません)
     user.Id = new UserId("1"); // dummy
     user.Name = name;
