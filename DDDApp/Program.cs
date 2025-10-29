02c5b8ce93a71312d2481401fd0702ae2f0a86a6
kakikubo <kakikubo@gmail.com>
2025-10-30 07:31:44 +0900

    UserとUserIdクラスを追加定義。Program.csからUserServiceを呼び出すところまでを仮実装

--- DDDApp/Program.cs
+++ DDDApp/Program.cs
@@ -57,3 +57,31 @@ Console.WriteLine(modelNumber);
 var validUserName= new UserName("kakikubo");
 Console.WriteLine(validUserName);
 var invalidUserName = new UserName("tk"); // Exceptionを吐く
+
+
+// ユーザ作成処理
+partial class Program
+{
+    public void CreateUser(string userName)
+    {
+        var user = new User(
+            new UserName(userName)
+        );
+        var userService = new UserService();
+        if (userService.Exists(user))
+        {
+            throw new Exception($"{userName}は既に存在しています");
+        }
+
+        var connectionString = ConfigurationManager.ConnectionStrings["FooConnection"].ConnectionString;
+        using (var connection = new SqlConnection(connectionString));
+        using (var command = connection.CreateCommand())
+        {
+            connection.Open();
+            command.CommandText = "INSERT INTO users (id, name) VALUES(@id, @name)";
+            command.Parameters.Add(new SqlParameter("@id", user.Id.Value));
+            command.Parameters.Add(new SqlParameter("@name", user.Name.Value));
+            command.ExecuteNonQuery();
+        }
+    }
+}
