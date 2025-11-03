fcd696dc80ccf69add7087a2969ac599018d9d4d
kakikubo <kakikubo@gmail.com>
2025-11-03 15:22:09 +0900

    EntityFrameworkが直接利用するデータモデル

---
+++ DDDApp/UserDataModel.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+[Table("Users")]
+public class UserDataModel
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
+    public string Id { get; set; }
+    
+    [Required]
+    [MinLength(3)]
+    public string Name { get; set; }
+}
