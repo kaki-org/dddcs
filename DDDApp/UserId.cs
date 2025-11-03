0dde75b35304a3951b5e0f4f5175e590ecf3a4ae
kakikubo <kakikubo@gmail.com>
2025-11-04 07:04:43 +0900

    Userクラスが利用している値オブジェクトの定義

--- DDDApp/UserId.cs
+++ DDDApp/UserId.cs
@@ -2,10 +2,9 @@ public class UserId
 {
     public UserId(string value)
     {
-        if (value == null) throw new ArgumentNullException(nameof(value));
+        if (string.IsNullOrEmpty(value)) throw new ArgumentNullException(nameof(value));
 
         Value = value;
     }
-
     public string Value { get; }
 }
--- DDDApp/UserName.cs
+++ DDDApp/UserName.cs
@@ -1,16 +1,13 @@
 public class UserName
 {
-    public string Value { get; }
-
     public UserName(string value)
     {
         if (value == null) throw new ArgumentNullException(nameof(value));
         if (value.Length < 3) throw new ArgumentOutOfRangeException("ユーザ名は3文字以上です", nameof(value));
+        if (value.Length > 20) throw new ArgumentOutOfRangeException("ユーザ名は20文字以下です", nameof(value));
+
         Value = value;
     }
 
-    public override string ToString()
-    {
-        return Value;
-    }
+    public string Value { get;  }
 }
