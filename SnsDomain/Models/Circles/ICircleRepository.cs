b199c706c7943232793842d5d6075c8744b65b64
kakikubo <kakikubo@gmail.com>
2025-11-14 07:29:07 +0900

    サークルのリポジトリ

---
+++ SnsDomain/Models/Circles/ICircleRepository.cs
@@ -0,0 +1,9 @@
+namespace SnsDomain.Models.Circles
+{
+    public interface ICircleRepository
+    {
+        void Save(Circle circle);
+        Circle Find(CircleId id);
+        Circle Find(CircleName name);
+    }
+}
