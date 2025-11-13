aa5166dc87fcda0a768cffbc149ed054d1548276
kakikubo <kakikubo@gmail.com>
2025-11-14 07:13:57 +0900

    サークルの識別子となる値オブジェクトを作成

---
+++ SnsDomain/Models/CircleId.cs
@@ -0,0 +1,14 @@
+namespace SnsDomain.Models.Circles
+{
+    public class CircleId
+    {
+        public CircleId(string value)
+        {
+            if (value == null) throw new ArgumentNullException(nameof(value));
+
+            Value = value;
+        }
+
+        public string Value { get; }
+    }
+}
