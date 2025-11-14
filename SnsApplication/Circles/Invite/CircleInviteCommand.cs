7ec15eb0317adf0e4f5155b01285b228b16be53d
kakikubo <kakikubo@gmail.com>
2025-11-15 07:56:35 +0900

    プロパティ名を修正

--- SnsApplication/Circles/Invite/CircleInviteCommand.cs
+++ SnsApplication/Circles/Invite/CircleInviteCommand.cs
@@ -4,13 +4,13 @@ namespace SnsApplication.Circles.Invite
     {
         public string CircleId { get; set; }
         public string FromUserId { get; set; }
-        public string InviteUserId { get; set; }
+        public string InvitedUserId { get; set; }
 
-        public CircleInviteCommand(string circleId, string fromUserId, string inviteUserId)
+        public CircleInviteCommand(string circleId, string fromUserId, string invitedUserId)
         {
             CircleId = circleId;
             FromUserId = fromUserId;
-            InviteUserId = inviteUserId;
+            InvitedUserId = invitedUserId;
         }
     }
 }
