86be00a4ade3f23cc45e8c9f976336edb08e9a05
kakikubo <kakikubo@gmail.com>
2025-11-15 05:51:21 +0900

    アプリケーションサービスにサークル作成処理を追加する

---
+++ SnsDomain/Models/Circles/CircleApplicationService.cs
@@ -0,0 +1,48 @@
+namespace SnsDomain.Models.Circles
+{
+    public class CircleApplicationService
+    {
+        private readonly ICircleFactory circleFactory;
+        private readonly ICircleRepository circleRepository;
+        private readonly CircleService circleService;
+        private readonly IUserRepositoy  userRepositoy;
+
+        public CircleApplicationService(
+            ICircleFactory circleFactory,
+            ICircleRepository circleRepository,
+            CircleService circleService,
+            IUserRepository userRepository
+        )
+        {
+            this.circleFactory = circleFactory;
+            this.circleRepository = circleRepository;
+            this.circleService = circleService;
+            this.userRepositoy = userRepository;
+        }
+
+        public void Create(CircleCreateCommand command)
+        {
+            using (var transaction = new TransactionScope())
+            {
+                var ownerId = new UserId(command.UserId);
+                var owner = userRepository.Find(ownerId)
+                if (owner == null)
+                {
+                    throw new UserNotFoundException(ownerId, "サークルのオーナーとなるユーザが見つかりませんでした。");
+                }
+
+                var name = new CircleName(command.Name);
+                var circle = circleFactory.Create(name, owner);
+                if (circleService.Exists(circle))
+                {
+                    throw new CanNotRegisterCircleException(circle, "サークルはすでに存在しています")
+                }
+
+                circleRepository.Save(circle);
+                transaction.Complete();
+            }
+        }
+
+    }
+    
+}
