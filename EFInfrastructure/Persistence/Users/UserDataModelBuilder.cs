127ae071f81bb9811245992992b21d8bb03456f0
kakikubo <kakikubo@gmail.com>
2025-11-16 07:16:56 +0900

    IUserNotificationを実装した通知オブジェクト

---
+++ EFInfrastructure/Persistence/Users/UserDataModelBuilder.cs
@@ -0,0 +1,33 @@
+using EFInfrastructure.Persistence.DataModels;
+using SnsDomain.Models.Users;
+
+namespace EFInfrastructure.Persistence.Users
+{
+    public class UserDataModelBuilder : IUserNotification
+    {
+        // 通知されたデータはインスタンス変数で保持される
+        private UserId id;
+        private UserName name;
+
+        public void Id(UserId id)
+        {
+            this.id = id;
+        }
+
+        public void Name(UserName name)
+        {
+            this.name = name;
+        }
+
+        // 通知されたデータからデータモデルを生成するメソッド
+        public UserDataModel Build()
+        {
+            return new UserDataModel
+            {
+                Id = id.Value,
+                Name = name.Value
+            };
+        }
+    }
+
+}
