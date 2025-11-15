620a01ef1f8db010f17c3728cc1b6b427dcbd394
kakikubo <kakikubo@gmail.com>
2025-11-16 07:26:04 +0900

    feat: Userクラスのカプセル化を強化し、通知機能を追加
    - プロパティをプライベートフィールドに変更してデータアクセスを制限
    - IUserNotificationを使用したNotifyメソッドを実装
    - ドメインモデルの整合性とデータ保護を向上

--- SnsDomain/Models/Users/User.cs
+++ SnsDomain/Models/Users/User.cs
@@ -3,25 +3,30 @@ namespace SnsDomain.Models.Users
     public class User
     {
         // インスタンス変数はいずれも非公開
-        public UserId Id { get; }
-        public UserName Name { get; private set; }
+        private readonly UserId id;
+        private UserName name;
 
         public User(UserId id, UserName name)
         {
             if (id == null) throw new ArgumentNullException(nameof(id));
             if (name == null) throw new ArgumentNullException(nameof(name));
 
-            Id = id;
-            Name = name;
+            this.id = id;
+            this.name = name;
         }
-
 
         public void ChangeName(UserName name)
         {
             if (name == null) throw new ArgumentNullException(nameof(name));
 
-            Name = name;
+            this.name = name;
         }
 
+        public void Notify(IUserNotification note)
+        {
+            // 内部データを通知
+            note.Id(id);
+            note.Name(name);
+        }
     }
 }
